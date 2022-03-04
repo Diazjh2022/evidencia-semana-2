85deb642dfe377fc9af8cdbaa22b615a4f881348
Jose Diaz <jdiazmontenegro2019@gmail.com>
2022-03-04 17:42:06 -0300

    se agrega boton despedir, y toas Asociado

--- app/src/main/java/com/example/ejerciciogit/MainActivity.java
+++ app/src/main/java/com/example/ejerciciogit/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btnSaludar;
+    Button btnDespedir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +17,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnSaludar = findViewById(R.id.btnSaludar);
+        btnDespedir =findViewById(R.id.btnDespedir);
         btnSaludar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "Hola mundo", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnDespedir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Chao nomas", Toast.LENGTH_SHORT).show();
             }
         });
     }
